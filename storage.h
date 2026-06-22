568eae953cfa471ed2c5faed865ba4c9bf2c2a46
s2520 <s2520@oita.kosen-ac.jp>
2026-06-22 20:00:25 +0900

    storage.hを修正

--- storage.h
+++ storage.h
@@ -1,9 +1,35 @@
 #ifndef STORAGE_H
 #define STORAGE_H
 
-#include "logic.h"
+#include "task.h"
 
-void save_tasks(Task list[], int count);
-void load_tasks(Task list[], int *count);
+
+
+/*
+ * 課題一覧を保存する
+ *
+ * 成功:1
+ * 失敗:0
+ */
+int save_tasks(const Task list[], int count);
+
+
+
+/*
+ * 課題一覧を読み込む
+ *
+ * 成功:1
+ * 失敗:0
+ */
+int load_tasks(Task list[], int *count);
+
+
+
+/*
+ * 次に使用するIDを取得する
+ */
+int get_next_id(const Task list[], int count);
+
+
 
 #endif
