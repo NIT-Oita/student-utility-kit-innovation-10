470feacacddfada4145118bd690edb246acd9c45
s2520 <s2520@oita.kosen-ac.jp>
2026-06-23 09:13:47 +0900

    stprage.hを変更

--- storage.h
+++ storage.h
@@ -1,10 +1,6 @@
 #ifndef STORAGE_H
 #define STORAGE_H
-
 #include "task.h"
-
-
-
 /*
  * 課題一覧を保存する
  *
@@ -12,9 +8,6 @@
  * 失敗:0
  */
 int save_tasks(const Task list[], int count);
-
-
-
 /*
  * 課題一覧を読み込む
  *
@@ -22,14 +15,8 @@ int save_tasks(const Task list[], int count);
  * 失敗:0
  */
 int load_tasks(Task list[], int *count);
-
-
-
 /*
  * 次に使用するIDを取得する
  */
 int get_next_id(const Task list[], int count);
-
-
-
 #endif
